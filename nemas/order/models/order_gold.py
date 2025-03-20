ea29724a957033d86810491bdf72acb731f19108
Chris <chris_budi@outlook.com>
2025-03-20 12:50:24 +0700

    update

--- nemas/order/models/order_gold.py
+++ nemas/order/models/order_gold.py
@@ -1,15 +1,17 @@
-from itertools import product
-from operator import truediv
-from pickle import TRUE
 from django.conf import settings
 
 from django.db import models
 from core.fields.uuidv7_field import UUIDv7Field
-from core.domain.address import postal_code
-from core.domain import gold, gold_price_config, cert, delivery_partner
+from core.domain import (
+    gold,
+    gold_price_config,
+    cert,
+    delivery_partner,
+    delivery_partner_service,
+    payment_method,
+)
 from core.domain.delivery import delivery_partner_service
 from order.models.order_cart import order_cart_detail
-from order.models import order_payment
 from user.models.users import user_address
 
 
@@ -33,7 +35,11 @@ class order_gold(models.Model):
 
     order_phone_number = models.CharField(max_length=255)
     order_item_weight = models.DecimalField(max_digits=10, decimal_places=4)
-    order_payment_method = models.CharField(max_length=255)
+
+    order_payment_method = models.ForeignKey(
+        payment_method, on_delete=models.CASCADE, null=True
+    )
+
     order_payment_va_bank = models.CharField(max_length=255, null=True)
     order_payment_va_number = models.CharField(max_length=255, null=True)
 
