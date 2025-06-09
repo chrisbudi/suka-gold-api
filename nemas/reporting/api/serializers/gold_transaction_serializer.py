e25d15df54ec5dd83d3190605733eddc58beea9b
chris_budi <chris_budi@outlook.com>
2025-06-09 21:12:38 +0700

    update

--- nemas/reporting/api/serializers/gold_transaction_serializer.py
+++ nemas/reporting/api/serializers/gold_transaction_serializer.py
@@ -10,7 +10,13 @@ class GoldTransactionContractSerializer(serializers.Serializer):
     transaction_date = serializers.CharField()
     transaction_id = serializers.CharField()
     weight = serializers.DecimalField(allow_null=True, max_digits=10, decimal_places=4)
+    admin_weight = serializers.DecimalField(
+        allow_null=True, max_digits=10, decimal_places=4
+    )
     price = serializers.DecimalField(allow_null=True, decimal_places=2, max_digits=16)
+    admin_price = serializers.DecimalField(
+        allow_null=True, decimal_places=2, max_digits=16
+    )
     gold_history_price_base = serializers.FloatField(allow_null=True)
     ref_number = serializers.CharField()
     transaction_type = serializers.CharField()
--- nemas/reporting/contracts/gold_transaction.py
+++ nemas/reporting/contracts/gold_transaction.py
@@ -7,6 +7,8 @@ class GoldTransactionContract:
     email: str
     user_id: int
     user_name: str
+    admin_price: float
+    admin_weight: float
     transaction_date: str
     transaction_id: int
     weight: float
