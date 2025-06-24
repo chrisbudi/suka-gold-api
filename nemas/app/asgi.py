9e0af3469b3763e99a7f1a5c3ee482fc58692a72
Chris Wang <chris_budi@outlook.com>
2025-06-25 01:26:59 +0700

    update web socket

--- nemas/app/asgi.py
+++ nemas/app/asgi.py
@@ -2,14 +2,25 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import OriginValidator
 from notification.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 print("ASGI application is starting...")
 
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-    }
+application = (
+    ProtocolTypeRouter(
+        {
+            "http": get_asgi_application(),
+            "websocket": OriginValidator(
+                AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+                [
+                    "null",
+                    "localhost",
+                    "http://www.nemas.id",
+                    "https://www.nemas.id/",
+                ],
+            ),
+        },
+    ),
 )
