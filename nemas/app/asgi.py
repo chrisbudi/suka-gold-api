1e3289a9743f2fdac8d5dee5d8ca66bb92d7f75f
Chris Wang <chris_budi@outlook.com>
2025-06-25 01:30:53 +0700

    update

--- nemas/app/asgi.py
+++ nemas/app/asgi.py
@@ -8,19 +8,17 @@ from notification.routing import websocket_urlpatterns
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 print("ASGI application is starting...")
 
-application = (
-    ProtocolTypeRouter(
-        {
-            "http": get_asgi_application(),
-            "websocket": OriginValidator(
-                AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-                [
-                    "null",
-                    "localhost",
-                    "http://www.nemas.id",
-                    "https://www.nemas.id/",
-                ],
-            ),
-        },
-    ),
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": OriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+            [
+                "null",
+                "localhost",
+                "http://www.nemas.id",
+                "https://www.nemas.id/",
+            ],
+        ),
+    }
 )
