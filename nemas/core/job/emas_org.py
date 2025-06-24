529a92c6bd0dc5efaefba5a837454987f1fe4507
Chris Wang <chris_budi@outlook.com>
2025-06-25 01:18:16 +0700

    update notification

--- nemas/core/job/emas_org.py
+++ nemas/core/job/emas_org.py
@@ -5,8 +5,12 @@ from datetime import datetime
 from core.domain import gold_price_config, gold_price_source
 from asgiref.sync import sync_to_async
 from core.services.price_service import price_service
+from notification.application.commands import notify_user
+from notification.application.dto import NotificationDTO
 from shared.services.redis_service import redis_service
 import math
+
+from user.models import user_notification_price
 
 
 class HargaEmasSpider(scrapy.Spider):
@@ -70,6 +74,38 @@ class HargaEmasSpider(scrapy.Spider):
                 timestamps=datetime.now(),
             )
             gprice.save()
+
+            # get all user notification price buy and sell
+            # where price_buy <  and price_sell
+            user_notification_prices = user_notification_price.objects.filter(
+                user_notification_price_buy__gte=price_buy,
+                user_notification_price_sell__lte=price_sell,
+                user_notification_price_status=True,
+            )
+            # if not user_notification_prices:
+            if not user_notification_prices.exists():
+                print("No user notification price found for the given criteria.")
+                return
+
+            # loop each user notification price
+            for user_notification_price in user_notification_prices:
+                user = user_notification_price.user
+                print(
+                    f"User: {user.username} - Price Buy: {user_notification_price.user_notification_price_buy} - Price Sell: {user_notification_price.user_notification_price_sell}"
+                )
+                dto = NotificationDTO(
+                    user_id=user.id,
+                    user_name=getattr(user, "username", None),
+                    user_email=getattr(user, "email", None),
+                    title="Notifikasi Harga Emas",
+                    message=f"Harga emas telah diperbarui. Harga beli: {price_buy}, Harga jual: {price_sell}",
+                    data={
+                        "price_buy": price_buy,
+                        "price_sell": price_sell,
+                    },
+                )
+                notify_user(dto)
+            # publish price to latest price socket
 
             # create object in with price buy and sell then publish it into websocket consumer
             price = {
